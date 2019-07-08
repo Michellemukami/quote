a86badc131c95b9e8f2cc1f3ccb8813f34aebc9c
Michelle Mukami <you@example.com>
2019-07-08 17:51:07 +0300

    changes made in Quotes.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,5 @@
 export class Quote {
+    
     public showDesc:boolean;
     constructor(public id:number, public quote:string,public author:string, public publisher:string,public upvotes:number,public downVote:number,public completeDate: Date){
         this.showDesc=false
