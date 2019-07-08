343058379031f7f0c1e658cd3295a5b636a4eebb
Michelle Mukami <you@example.com>
2019-07-08 17:45:28 +0300

    add created folder

--- src/app/quote-form/quote-form.component.spec.ts
+++ src/app/quote-form/quote-form.component.spec.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote';
 
-@Component({
-  selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.scss']
-})
-export class QuoteFormComponent implements OnInit {
-  
-  newQuote =new Quote(0,"","","",0,0,new Date());
-  @Output() addQuote = new EventEmitter<Quote>();
-
-  quoteSubmission(){
-    this.addQuote.emit(this.newQuote);
-    this.newQuote=new Quote(0,"","","",0,0,new Date());
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { QuoteFormComponent } from './quote-form.component';
+describe('QuoteFormComponent', () => {
+ let component: QuoteFormComponent;
+ let fixture: ComponentFixture<QuoteFormComponent>;
+ beforeEach(async(() => {
+   TestBed.configureTestingModule({
+     declarations: [ QuoteFormComponent ]
+   })
+   .compileComponents();
+ }));
+ beforeEach(() => {
+   fixture = TestBed.createComponent(QuoteFormComponent);
+   component = fixture.componentInstance;
+   fixture.detectChanges();
+ });
+ it('should create', () => {
+   expect(component).toBeTruthy();
+ });
+});
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDesc:boolean;
-    constructor(public id:number, public quote:string,public author:string, public publisher:string,public upvotes:number,public downVote:number,public actualDate:Date){
+    constructor(public id:number, public quote:string,public author:string, public publisher:string,public upvotes:number,public downVote:number,public completeDate: Date){
         this.showDesc=false
     }
     
