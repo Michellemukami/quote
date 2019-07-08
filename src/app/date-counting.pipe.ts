2a93724926c2ab565104322d94c7cb4e54442658
Michelle Mukami <you@example.com>
2019-07-08 17:53:32 +0300

    changes made in date-counting.pipe.ts

--- src/app/date-counting.pipe.ts
+++ src/app/date-counting.pipe.ts
@@ -7,7 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
-
     let today:Date = new Date();
     let todayWithoutTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
 
