0f7ea54e6f004951e3639e300f777a1b921a6213
Michelle Mukami <you@example.com>
2019-07-08 17:53:51 +0300

    changes made in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
